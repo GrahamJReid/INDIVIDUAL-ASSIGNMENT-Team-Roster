f6d878fa8a6f4c1504e8c1d25f38770ec1ced3a2
GrahamJReid <grahamreidtechnology@gmail.com>
2023-01-24 14:12:23 -0600

    nav-bar styled

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,33 +8,35 @@ import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="navBar">
-      <Container>
-        <Link passHref href="/">
-          <Navbar.Brand>Team Central</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Link passHref href="/teams">
-              <Nav.Link>Teams</Nav.Link>
-            </Link>
-            <Link passHref href="/newteams">
-              <Nav.Link>Create Team</Nav.Link>
-            </Link>
-            <Link passHref href="/new">
-              <Nav.Link>Add A Member</Nav.Link>
-            </Link>
-            <Link passHref href="/">
-              <Button variant="danger" onClick={signOut}>Sign Out</Button>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="nav-bar-container">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="navBar">
+        <Container>
+          <Link passHref href="/">
+            <Navbar.Brand className="nav-title">Team Central</Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
+              <Link passHref href="/">
+                <Nav.Link className="nav-link">Home</Nav.Link>
+              </Link>
+              <Link passHref href="/teams">
+                <Nav.Link className="nav-link">Teams</Nav.Link>
+              </Link>
+              <Link passHref href="/newteams">
+                <Nav.Link className="nav-link">Create Team</Nav.Link>
+              </Link>
+              <Link passHref href="/new">
+                <Nav.Link className="nav-link">Add A Member</Nav.Link>
+              </Link>
+              <Link passHref href="/">
+                <Button className="nav-bar-signout" onClick={signOut}>Sign Out</Button>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
